b426615c26fc72a3d971f122a90aba7c867c7300
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-12-31 19:59:42 -0400

    feat: add google auth and save the user in mongodb

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,17 +6,17 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 function Nav() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
       setProviders(response);
     };
-    // setProviders();
+    setUpProviders();
   }, []);
 
   return (
@@ -33,7 +33,7 @@ function Nav() {
       </Link>
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -43,7 +43,7 @@ function Nav() {
             </button>
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user?.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -69,10 +69,10 @@ function Nav() {
       </div>
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user?.image}
               width={37}
               height={37}
               className="rounded-full"
--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+"use client";
 
-function Provider() {
-  return (
-    <div>Provider</div>
-  )
+import { SessionProvider } from "next-auth/react";
+
+function Provider({ children, session }) {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 }
 
-export default Provider
+export default Provider;
